948d4cf36d96a9a9605734ef9b881b60261304e3
Chris Bohl <chrisrbohl@gmail.com>
2015-05-03 20:35:05 -0500

    Cleaned up fake codecademy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,60 +2,15 @@ class WelcomeController < ApplicationController
 
   def index
     require 'Open3'
-    sent_code = params[:code]
+    @input = params[:code]
+    stdin, stdout, stderr = Open3.popen3("ruby -e '#{@input}'")
 
+    @output = stdout.readlines
+    @errors = stderr.readlines
 
-    p sent_code
-
-    if sent_code
-      puts "INPUT"
-      p sent_code
-
-      # alterred_code = sent_code.gsub(/\r\n/, "; ")
-
-      # puts "ALTERED INPUT"
-      # p alterred_code
-
-      puts "OUTPUT"
-      # @output = IO.popen("ruby -e '#{alterred_code}'").readlines
-
-      # @output = IO.popen("ruby -e '#{sent_code}'").readlines
-      @stdin, @stdout, @stderr = Open3.popen3("ruby -e '#{sent_code}'")
-
-      @output = @stdout.readlines
-
-      if @output
-        # p @output[0]
-        puts
-        p @output.to_s
-        @output.map! do |line|
-          line.gsub(/\n/, "")
-        end
-        puts "OUTPUT"
-        p @output
-        puts "OTHER"
-        p @output[0]
-        p "#{@output[0]}".to_s
-
-        puts "ERROR MESSAGE"
-        @errors = @stderr.readlines
-      end
-      # if @output != nil
-      #   @output = @output[0].gsub(/\//, "")
-      # end
+    @output.map! do |line|
+      line.gsub(/\n/, "")
     end
-
-
   end
+
 end
-      # @output = eval(sent_code)
-  # puts output.readlines
-    # puts "SENT CODE"
-    # p sent_code
-    # puts "OUTPUT"
-    # p @output
-    # puts "DONE"
-    # STDOUT
-    # @output.map! do |line|
-    #   line = line.gsub(/\//, "")
-    # end
